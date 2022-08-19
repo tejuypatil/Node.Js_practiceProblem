c88093bc9c87f8d770b3bb50a1bbe7828c8844df
tejuypatil <tejuypatil@gmail.com>
2022-08-19 21:39:47 +0530

    Added Program of read the single digit number and write in word

---
+++ Selection_Practice_problem_with_ifelse.js
@@ -0,0 +1,47 @@
+//Read a single digit number and write the number in word
+
+
+let randomCheck = (Math.floor(Math.random()*10)%10);
+if(randomCheck == 1)
+{
+    console.log("\n"+randomCheck+"one");
+}
+else if(randomCheck == 2)
+{
+    console.log("\n"+randomCheck+"TWO");
+}
+else if (randomCheck == 3)
+{
+    console.log("\n"+randomCheck+"THREE");
+}
+else if (randomCheck == 4)
+{
+    console.log("\n"+randomCheck+"FOUR");
+}
+else if (randomCheck == 5)
+{
+    console.log("\n"+randomCheck+"Five");
+}
+else if (randomCheck == 6)
+{
+    console.log("\n"+randomCheck+"SIX");
+}
+else if (randomCheck == 7)
+{
+    console.log("\n"+randomCheck+"SEVEN");
+}
+else if (randomCheck == 8)
+{
+    console.log("\n"+randomCheck+"EIGHT");
+}
+else if (randomCheck == 9)
+{
+    console.log("\n"+randomCheck+"NINE");
+}
+else if (randomCheck == 0)
+{
+    console.log("\n"+randomCheck+"ZERO");
+}
+else{
+    
+}
